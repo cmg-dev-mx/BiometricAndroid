4464ff6ce132d40e0e08a2b4ab5b75cfbb7ffa9e
Cesar Morales Garduño <cmg.dev.mx@gmail.com>
2026-01-30 09:57:55 -0700

    Adición comportamiento on next en teclado en login

--- Android/Biometrics/app/src/main/java/mx/dev/cmg/android/biometrics/ui/feature/login/layout/LoginLayout.kt
+++ Android/Biometrics/app/src/main/java/mx/dev/cmg/android/biometrics/ui/feature/login/layout/LoginLayout.kt
@@ -5,14 +5,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +30,8 @@ fun LoginLayout(
     onEvent: (LoginEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val passwordFocusRequester = remember { FocusRequester() }
+
     Column(
         modifier = modifier.padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -37,16 +44,26 @@ fun LoginLayout(
             modifier = Modifier.fillMaxWidth(),
             value = uiState.user,
             onValueChange = { onEvent(LoginEvent.OnUserInputChanged(it)) },
-            label = { Text("User") }
+            label = { Text("User") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(
+                onNext = { passwordFocusRequester.requestFocus() }
+            )
         )
         TextField(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(passwordFocusRequester),
             value = uiState.pass,
             onValueChange = { onEvent(LoginEvent.OnPasswordInputChanged(it)) },
             label = { Text("Password") },
             keyboardOptions = KeyboardOptions.Default.copy(
                 autoCorrectEnabled = false,
-                keyboardType = KeyboardType.Password
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { onEvent(LoginEvent.OnLoginButtonClicked) }
             )
         )
 
